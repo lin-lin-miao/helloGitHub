963c48c43202e24e662128f532ebf4caafe5a218
met-lin <linjiajun.233@qq.com>
2022-07-28 01:35:22 +0800

    1

--- useGit.java
+++ useGit.java
@@ -2,7 +2,7 @@ public class useGit {
 
     public static void main(String[] args) {
         
-        System.out.println("建立git仓库");
+        System.out.println("建立git仓库,哈哈");
         System.out.println("git int");
     }
 
